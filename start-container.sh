0a3c2a0c350e03fba6bcf2bf80f8f57b9b579f9d
rykahsay <rykahsay@gwu.edu>
2022-11-29 20:01:28 -0500

    Towards version 2.0

--- start-container.sh
+++ start-container.sh
@@ -6,17 +6,9 @@ port="8082"
 
 python3 setup.py bdist_wheel 
 
-##################################################################
-# maintain customized versions of the config file for each server
-# following instance/config.dev.py, and keep these versions in 
-# private folder
-##############################################################
-
-cp private/config.glygen_$server.py instance/config.py 
 docker build --network=host -t $image .
-rm instance/config.py
 
 docker rm -f $container
 
-sudo docker run -dit --name $container -p 127.0.0.1:$port:80 -v /data/shared/glygen:/data/shared/glygen $image
+docker run -dit --name $container -p 127.0.0.1:$port:80 -v /data/shared/glygen:/data/shared/glygen $image
 
