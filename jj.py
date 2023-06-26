1577720eace41692741e3e7a1d906083fea043c5
rykahsay <rykahsay@gwu.edu>
2023-06-26 11:09:31 -0400

    Towards version 2.1

--- jj.py
+++ jj.py
@@ -7,22 +7,12 @@ from bson import json_util
 import pymongo
 from pymongo import MongoClient
 import datetime
-import pytz
-import subprocess
+
 
 __version__="1.0"
 __status__ = "Dev"
 
-def get_coll_list(db_list):
-    coll_list = []
-    for d in db_list:
-        if d[-2:] != "db":
-            continue                                    
-        coll = "c_" + d[:-2]
-        if coll == "c_jumbo":
-            continue                                                                            
-        coll_list.append(coll)
-    return coll_list
+
 
 ###############################
 def main():
@@ -31,30 +21,53 @@ def main():
     usage = "\n%prog  [options]"
     parser = OptionParser(usage,version="%prog version___")
     parser.add_option("-s","--server",action="store",dest="server",help="dev/tst/beta/prd")
-    parser.add_option("-v","--dataversion",action="store",dest="dataversion",help="2.0.2/2.0.3 ...")
-        
+    parser.add_option("-c","--coll",action="store",dest="coll",help="") 
     (options,args) = parser.parse_args()
 
-    for key in ([options.server, options.dataversion]):
+    for key in ([options.server, options.coll]):
         if not (key):
             parser.print_help()
             sys.exit(0)
 
     server = options.server
-    ver = options.dataversion
-    mode = "full"
+    coll = options.coll
 
     config_obj = json.loads(open("./conf/config.json", "r").read())
     mongo_port = config_obj["dbinfo"]["port"][server]
+    mongo_container = "running_glygen_mongo_%s" % (server)
+
     host = "mongodb://127.0.0.1:%s" % (mongo_port)
+  
+    db_obj = config_obj["dbinfo"]["glydb"]
+    db_name, db_user, db_pass =  db_obj["db"], db_obj["user"], db_obj["password"]
 
-    jsondb_dir = config_obj["data_path"] + "/releases/data/v-%s/jsondb/" % (ver)
-    dump_dir = config_obj["data_path"] + "/mongodump/"
+    try:
+        client = pymongo.MongoClient(host,
+            username=db_user,
+            password=db_pass,
+            authSource=db_name,
+            authMechanism='SCRAM-SHA-1',
+            serverSelectionTimeoutMS=10000
+        )
+        client.server_info()
+        dbh = client[db_name]
+        prj = {"uniprot_canonical_ac":-1, "score":-1}
+        cutoff = 5.0
+        q = [ 
+            { "$match": { "$text": { "$search": "kinase isomerase" } } },
+            { "$addFields":{"score":{"$meta":"textScore"}}},
+            { "$match":{"score":{"$gt":cutoff}}},
+            { "$project" : prj }
+        ]
+        
+        for doc in dbh[coll].aggregate(q):
+            print (json.dumps(doc, indent=4, default=json_util.default))
 
-    db_list = config_obj["downloads"]["jsondb"]
-    coll_list = get_coll_list(db_list)
+    except pymongo.errors.ServerSelectionTimeoutError as err:
+        print (err)
+    except pymongo.errors.OperationFailure as err:
+        print (err)
 
-    print (coll_list)
 
 
 if __name__ == '__main__':
