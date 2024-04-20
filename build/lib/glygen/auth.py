5330b6e1ebb9c6b0a331ad0e8234075413c71b15
rykahsay <rykahsay@gwu.edu>
2024-04-20 05:34:08 -0400

    Updates towards 2.4

--- build/lib/glygen/auth.py
+++ build/lib/glygen/auth.py
@@ -166,8 +166,8 @@ class Auth(Resource):
                 error = "password does not exist for registerd user"
             else:
                 submitted_password = password.encode('utf-8')
-                stored_password = user_doc['password'].encode('utf-8')
-                #stored_password = user_doc['password']
+                #stored_password = user_doc['password'].encode('utf-8')
+                stored_password = user_doc['password']
                 if bcrypt.hashpw(submitted_password, stored_password) != stored_password:
                     error = "incorrect-email/password"
             res_obj = log_request(req_obj, "/auth/login/", request)
--- build/lib/glygen/biomarker_apilib.py
+++ build/lib/glygen/biomarker_apilib.py
@@ -10,7 +10,7 @@ from bson import json_util, ObjectId
 
 
 from glygen.db import get_mongodb
-from glygen.util import get_errors_in_query, sort_objects, order_obj, clean_obj, get_paginated_sections
+from glygen.util import get_errors_in_query, sort_objects, order_obj, clean_obj, get_paginated_sections, cache_record_list
 
 
 
@@ -29,7 +29,7 @@ def biomarker_detail(query_obj, config_obj):
         return {"error_list":error_list}
     collection = "c_biomarker"
 
-    mongo_query = {"biomarker_id":{"$eq":query_obj["id"]}}
+    mongo_query = {"biomarker_canonical_id":{"$eq":query_obj["biomarker_canonical_id"]}}
     obj = dbh[collection].find_one(mongo_query)
     #check for post-access error, error_list should be empty upto this line
     post_error_list = []
@@ -55,6 +55,67 @@ def biomarker_detail(query_obj, config_obj):
     obj["crossref"] = []
 
     return obj
+
+
+
+
+def biomarker_search(query_obj, config_obj):
+
+    dbh, error_obj = get_mongodb()
+    if error_obj != {}:
+        return error_obj
+
+    #Collect errors 
+    error_list = get_errors_in_query("biomarker_search", query_obj,config_obj)
+    if error_list != []:
+        return {"error_list":error_list}
+
+    mongo_query = get_mongo_query(query_obj)
+    #return mongo_query
+
+
+    collection = "c_biomarker"
+    record_list = []
+    record_type = "biomarker"
+    prj_obj = {"biomarker_canonical_id":1}
+    for obj in dbh[collection].find(mongo_query,prj_obj):
+        record_list.append(obj["biomarker_canonical_id"])
+
+    ts_format = "%Y-%m-%d %H:%M:%S %Z%z"
+    ts = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime(ts_format)
+    cache_coll = "c_cache"
+    list_id = ""
+    if len(record_list) != 0:
+        hash_str = record_type + "_" + json.dumps(query_obj)
+        hash_obj = hashlib.md5(hash_str.encode('utf-8'))
+        list_id = hash_obj.hexdigest()
+        cache_info = {
+            "query":query_obj,
+            "ts":ts,
+            "record_type":record_type,
+            "search_type":"search"
+        }
+        cache_record_list(dbh,list_id,record_list,cache_info,cache_coll,config_obj)
+    res_obj = {"list_id":list_id}
+
+    return res_obj
+
+
+
+
+def get_mongo_query(query_obj):
+
+                        
+    cond_objs = []
+    if "biomarker_canonical_id" in query_obj:
+        q_id = query_obj["biomarker_canonical_id"]
+        cond_objs.append({"biomarker_canonical_id":{'$regex': q_id, '$options': 'i'}})
+
+
+    operation = query_obj["operation"].lower() if "operation" in query_obj else "and"
+    mongo_query = {} if cond_objs == [] else { "$"+operation+"": cond_objs }
+       
+    return mongo_query
 
 
 
